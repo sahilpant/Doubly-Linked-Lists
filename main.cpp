60b43b1bf0a2e6b95255a9016784632274012f9c
Sahil Pant <sahilpant16@hotmail.com>
2020-06-24 16:53:43 +0530

    added features

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ struct DLLNode
 
 typedef struct DLLNode node;
 
-void create(){
+void CreateList(){
     int data;
     node *NewNode;
     NewNode = new node;
@@ -21,7 +21,7 @@ void create(){
     head = NewNode;
 }
 
-void insertbeg(int data){
+void InsertBeg(int data){
     node *NewNode;
     NewNode = new node;
     NewNode->data = data;
@@ -50,14 +50,14 @@ void RandomInsert(int data,int pos){
         head = NewNode;
     }
     else{
-        while(p->right!=NULL&& ctr < pos){
+        while( p && ctr < pos ){
             q = p;
             p = p->right;
             ctr++;
         }
-        if(p->right==NULL){
-            p->right = NewNode;
-            NewNode->left = p;
+        if(q->right==NULL){
+            q->right = NewNode;
+            NewNode->left = q;
             NewNode->right = NULL;
         }
         else{
@@ -83,15 +83,144 @@ void InsertEnd(int data){
     NewNode->right = NULL;
 }
 
+void DeleteEnd()
+{
+    if(head == NULL)
+    {
+        cout<<"LIST IS EMPTY";
+    }
+    node *p, *q, *temp;
+    p = head;
+    while(p)
+    {
+        q = p;
+        p = p->right;
+    }
+    temp = q->left;
+    temp->right = NULL;
+    delete(q);
+}
+
+void DeleteBeg(){
+    if(head == NULL)
+    {
+        cout<<"LIST IS EMPTY";
+    }
+    node *temp;
+    temp = head;
+    head = head->right;
+    head->left = NULL;
+    delete(temp);
+}
+
+void DeleteRandom(int pos){
+    if(head == NULL)
+    {
+        cout<<"LIST IS EMPTY";
+    }
+    if(pos==1){
+        DeleteBeg();
+        return;
+    }
+    node *p, *q, *temp;
+    int ctr = 1;
+    p = head;
+    while(p && ctr < pos){
+        q = p;
+        p = p->right;
+        ctr++;
+    }
+    if(q->right==NULL){
+        DeleteEnd();
+        return;
+    }
+    else{
+        temp = p->right;
+        q->right = temp;
+        temp->left = q;
+    }
+}
+
+void Reverse() {
+    if(head == NULL)
+    {
+        cout<<"LIST IS EMPTY";
+    }
+    cout<<" ";
+}
 
 int main(){
-    create();
-    insertbeg(-1);
-    insertbeg(-2);
-    insertbeg(-3);
-    show();
-    cout<<"\n";
-    RandomInsert(2,5);
-    InsertEnd(3);
-    show();
+    char ch1,ch2;
+    int choice,data,position;
+    cout<<"Do you want to create a Doubly Linked List::";
+    cin>>ch1;
+    (ch1=='y'||ch1=='Y') ? CreateList() : exit(0);
+    do{
+        cout<<"\nchoose from the following options::";
+        cout<<"\n1.Insert at the end::";
+        cout<<"\n2.Insert at the Beginning::";
+        cout<<"\n3.Insert at a random position::";
+        cout<<"\n4.delete at the end::";
+        cout<<"\n5.delete at the beginning::";
+        cout<<"\n6.delete at any random position::";
+        cout<<"\n7.Display the list::";
+        cout<<"\n8.reverse the list::";
+        cout<<"\nEnter::";
+        cin>>choice;
+        switch(choice)
+        {
+            case 1:
+            cout<<"\nEnter the Data::";
+            cin>>data;
+            InsertEnd(data);
+            break;
+            
+            case 2:
+            cout<<"\nEnter the Data::";
+            cin>>data;
+            InsertBeg(data);
+            break;
+
+            case 3:
+            
+            cout<<"\nEnter the Data and position::";
+            cin>>data>>position;
+            RandomInsert(data,position);
+            break;
+
+            case 4:
+            
+            cout<<"Data has been deleted==>";
+            DeleteEnd();
+            break;
+            
+            case 5:
+
+            DeleteBeg();
+            cout<<"\nData has been deleted==>";
+            break;
+
+            case 6:
+
+            cout<<"\nEnter the position::";
+            cin>>position;
+            DeleteRandom(position);
+            cout<<"\nData has been deleted==>";
+            break;
+
+            case 7:
+            
+            show();
+            
+            break;
+
+            case 8:
+
+            Reverse();
+            
+            break;
+        }
+        cout<<"\nDo You want to continue?(y/n)::";
+        cin>>ch2;
+    }while(ch2=='y'||ch2=='Y');
 }
